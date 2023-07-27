b1c2cd0eb631a0613794e8207b8d368683dbefcf
Davee <git@davee.pro>
2023-07-27 20:07:38 +0100

    Fix encryption mix column matrix order

--- src/encrypt.h
+++ src/encrypt.h
@@ -44,10 +44,10 @@ struct EncryptDFA
                                               { 2, 5, 8, 15 },
                                               { 3, 6, 9, 12 } };
 
-    static constexpr std::uint8_t mix_columns_matrix[4][4] = { { 2, 3, 1, 1 },
-                                                               { 1, 2, 3, 1 },
-                                                               { 1, 1, 2, 3 },
-                                                               { 3, 1, 1, 2 } };
+    static constexpr std::uint8_t mix_columns_matrix[4][4] = { { 2, 1, 1, 3 },
+                                                               { 3, 2, 1, 1 },
+                                                               { 1, 3, 2, 1 },
+                                                               { 1, 1, 3, 2 } };
 };
 
 std::array<std::uint8_t, 256> EncryptDFA::inverse_box = { aes_isbox };
